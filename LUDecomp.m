2fdd40d96a980a38817aa70d7f1c2bf0a2c8efbf
Jia Xun <chaijiaxun@gmail.com>
2017-10-23 00:32:49 +0800

    Broken L code.

--- LUDecomp.m
+++ LUDecomp.m
@@ -1,6 +1,8 @@
 function M = LUDecomp()
-  A = [2 -3 1; 1 1 -1; -1 1 -3];
-  B = [-1 0 0];
+  % A = [2 -3 1; 1 1 -1; -1 1 -3] 
+  % B = [-1 0 0];
+  A = [0 1 -1 1; 1 1 -1 2; -1 -1 1 0; 1 2 0 2 ]
+  B = [3 4 -4 5];
   C = [A B'];
   [L, U] = LUD(A, B);
   M = [L U];
@@ -9,23 +11,29 @@ end
 function [L, U] = LUD(A, B)
   
   C = [A B'];
-  P = zeros(size(A))
-  L = zeros(size(A))
+  P = eye(size(A));
+  L = eye(size(A));
   
   
-  [cols rows] = size(A)
+  [cols rows] = size(A);
   for k = 1:cols
     [t p] = max(abs(C(k:cols, k)));
     p = p + k-1; % Convert to matrix
-    C([k p], :) = C([p k], :);
-    P([k p], :) = P([p k], :);
+    if p ~= k
+      C([k p], :) = C([p k], :);
+      P([k p], :) = P([p k], :);
+      L([k,p],:) = L([p,k],:);
+    end
     for r = k+1:cols
       % Get the multiplier
       mult = C(r,k) / C(k,k);
+      L(r, k) = mult;
       % Multiply the row by this thing
-      C(r,:) -= C(k,:) * mult;
+      C(r,k:cols) -= C(k,k:cols) * mult;
     end
   end
+  L * C
+  L
   
   % Loop down the columns
   % for c = 1:cols
